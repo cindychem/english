efd7b47845f638b8720cdfa4637347735dfb057f
cindychem <135210673+cindychem@users.noreply.github.com>
2023-06-12 23:04:28 +0800

    Update new.py

--- new.py
+++ new.py
@@ -1,3 +1,4 @@
+import re
 import requests
 from bs4 import BeautifulSoup
 
@@ -12,6 +13,13 @@ def read( word ):
         chinese = data
         phone = row
         s = " ".join( phone )
-        return( chinese + s )
+        text = chinese + s
+        regex= re.compile(r'[\u4E00-\u9FFF\uFF00-\uFFFF]|[a-zA-Z0-9]+')
+        arr = re.findall(regex, text)
+        text = ' '.join(arr)
+
+        regex= re.compile(r'(?<=[^a-zA-Z0-9\u0021-\u002E])(\x20)(?=[^a-zA-Z0-9\u0021-\u002E])')
+        text = re.sub(regex, '', text)
+        return( text )
     except:
         return( '查無此字' )
