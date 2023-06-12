0fb3fcb2717c2e65d088433b2e521aedf3510f48
cindychem <135210673+cindychem@users.noreply.github.com>
2023-06-12 08:28:58 +0800

    Update new.py

--- new.py
+++ new.py
@@ -1,17 +1,18 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = f'http://dict.cn/big5/{word}#searchL'
+def read( word ):
+    url = f'http://dict.cn/big5/{word}#searchL'
 
-html = requests.get( url )
-bs = BeautifulSoup(html.text,'lxml')
-data = bs.find('h1').text
-row = bs.find('strong').text
-try:
-    chinese = data
-    phones = row
-    s = " ".join( phones )
-    # s = row.find('sub')
-    return( s )
-except:
-    return( '查無此字' )
+    html = requests.get( url )
+    bs = BeautifulSoup(html.text,'lxml')
+    data = bs.find('h1').text
+    row = bs.find('strong').text
+    try:
+        chinese = data
+        phones = row
+        s = " ".join( phones )
+        # s = row.find('sub')
+        print( chinese + s )
+    except:
+        return( '查無此字' )
